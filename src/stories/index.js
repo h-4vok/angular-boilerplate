3c82a237cf2530f69f3973bc91bfc9db41ca4b50
Manuel Irazabal <manuelirazabal@hotmail.com>
2019-03-19 18:19:40 -0300

    components

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,7 +1,11 @@
 import { storiesOf } from '@storybook/angular';
 import { Button } from '@storybook/angular/demo';
+import { DashboardComponent } from '../app/dashboard/dashboard.component';
+import { MessagesComponent } from '../app/messages/messages.component';
+import { HeroesComponent } from '../app/heroes/heroes.component';
+import { HeroDetailComponent } from '../app/hero-detail/hero-detail.component';
 
-storiesOf('My Button', module)
+storiesOf('Button Example', module)
   .add('with emoji', () => ({
     component: Button,
     props: {
@@ -13,4 +17,22 @@ storiesOf('My Button', module)
     props: {
       text: '😀 😎 👍 💯',
     },
+  }));
+
+storiesOf('Components Example', module)
+  .add('Dashboard', () => ({
+    component: DashboardComponent,
+    props: {},
+  }))
+  .add('Message', () => ({
+    component: MessagesComponent,
+    props: { "messageService": { "messages": ["mensaje1", "mensaje2"] } },
+  }))
+  .add('heroes', () => ({
+    component: HeroesComponent,
+    props: {},
+  }))
+  .add('hero-detail', () => ({
+    component: HeroDetailComponent,
+    props: {},
   }));
