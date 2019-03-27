0dd4ad5f4d145624bf5451e31aecbf2632f7afbb
fsystemweb <contacto@fsystem.com.ar>
2019-03-27 10:46:10 -0300

    Remove example stories

--- .storybook/stories/dashboard.js
+++ .storybook/stories/dashboard.js
@@ -2,21 +2,11 @@ import { storiesOf } from '@storybook/angular';
 import { DashboardComponent } from 'src/app/screens/dashboard/dashboard.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { moduleMetadata } from '@storybook/angular';
-import { Button } from '@storybook/angular/demo';
 import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from 'src/app/app.component';
 import { HeroesComponent } from 'src/app/screens/heroes/heroes.component';
 import { HeroDetailComponent } from 'src/app/screens/hero-detail/hero-detail.component';
 import { APP_BASE_HREF } from '@angular/common';
-
-storiesOf('Button Example', module)
-
-  .add('with emoji', () => ({
-    component: Button,
-    props: {
-      text: '😀 😎 👍 💯',
-    },
-  }))
 
 storiesOf('Components Example', module)
   .addDecorator(
